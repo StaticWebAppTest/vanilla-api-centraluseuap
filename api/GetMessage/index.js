5569e90e32a3f4f567a42f5864bf7d36967b776a
Annaji Sharma Ganti <42851022+annaji-msft@users.noreply.github.com>
2021-02-11 23:29:09 -0800

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,12 +1,9 @@
 module.exports = async function (context, req) {
-  var siteName = 
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: {
       websiteName: process.env.WEBSITE_SITE_NAME,
-      package: process.env.WEBSITE_RUN_FROM_PACKAGE,
-      version: process.env.FUNCTIONS_EXTENSION_VERSION,
-      runtime: process.env.FUNCTIONS_WORKER_RUNTIME
+      package: process.env.WEBSITE_RUN_FROM_PACKAGE
     },
   };
 };
