727e7675f607b1d3518c37f58f504780a1819425
Annaji Sharma Ganti <42851022+annaji-msft@users.noreply.github.com>
2021-04-26 18:52:54 -0700

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
     body: {
-      text: `version:4 siteName: ${siteName} packageUrl: ${packageUrl}`
+      text: `version:5 siteName: ${siteName} packageUrl: ${packageUrl}`
     },
   };
 };
