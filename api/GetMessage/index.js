e444161dc8d224484a352842c41a960ef0124b30
Annaji Sharma Ganti <anganti@microsoft.com>
2021-05-07 11:01:03 -0700

    auto run

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,11 +1,1 @@
-module.exports = async function (context, req) {
-  var siteName = process.env.WEBSITE_SITE_NAME;
-  var packageUrl = process.env.WEBSITE_RUN_FROM_PACKAGE;
-  var packageUrl = process.env.WEBSITE_RUN_FROM_PACKAGE;
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: {
-      text: `version:1aa2 siteName: ${siteName} packageUrl: ${packageUrl}`
-    },
-  };
-};
+module.exports = async function (context, req) {  context.res = { body: { text:'2021-05-07 11:00:53.295806'},  };};
